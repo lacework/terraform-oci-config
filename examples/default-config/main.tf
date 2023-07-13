d23099cbb4c955ba4e489586ddcc62d0bc9cb221
Kolbeinn Karlsson <kolbeinn.karlsson@lacework.net>
2023-07-13 19:52:42 +0000

    style: format examples with tf fmt
    Signed-off-by: Kolbeinn Karlsson <kolbeinn.karlsson@lacework.net>

--- examples/default-config/main.tf
+++ examples/default-config/main.tf
@@ -1,6 +1,6 @@
 module "lacework_oci_cfg_integration" {
-  source = "../.."
-  create = true
+  source     = "../.."
+  create     = true
   tenancy_id = var.tenancy_ocid
   user_email = "example@example.com"
 }
--- examples/skip-creation-config/main.tf
+++ examples/skip-creation-config/main.tf
@@ -1,6 +1,6 @@
 module "lacework_oci_cfg_integration" {
-  source = "../.."
-  create = false
+  source     = "../.."
+  create     = false
   tenancy_id = var.tenancy_ocid
   user_email = "example@example.com"
 }
